fb588471d8bbbd53c1aad7862e71e43950d344a8
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-04-24 08:27:45 -0400

    add StateManagerTests

---
+++ src/test/java/br/org/synapse/manager/StateManagerTestCase.java
@@ -0,0 +1,19 @@
+package br.org.synapse.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StateManagerTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testInstanceNotNull() {
+		assertNotNull(StateManager.getStateManager());
+	} 
+
+}
