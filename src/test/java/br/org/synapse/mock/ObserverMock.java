6ae5f9971a486ab80579ec9555c3ab2f5057e625
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2013-04-24 13:55:31 -0400

    Add observerMock

---
+++ src/test/java/br/org/synapse/mock/ObserverMock.java
@@ -0,0 +1,18 @@
+package br.org.synapse.mock;
+
+import br.org.synapse.core.IObserver;
+import br.org.synapse.core.ISubject;
+
+public class ObserverMock implements IObserver{
+
+	@Override
+	public void changeNotify(ISubject sender) {
+		
+	}
+
+	@Override
+	public void destroyNotify(ISubject sender) {
+		
+	}
+
+}
